7af3e48590e241ac540c3fa65025ee8724ed7e41
Cmilja <dsmiljanic40@gmail.com>
2025-03-18 01:08:45 +0100

    Task factory and seeding

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(3),
+            'description' => fake()->text(100),
+            'status' => fake()->randomElement(['pending', 'completed'])
+        ];
+    }
+}
