1da138f09afa29153e5ee5dba559b469b2f9cf55
Cmilja <dsmiljanic40@gmail.com>
2025-03-19 23:09:57 +0100

    Routes

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -10,6 +10,8 @@ class Task extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['title', 'description', 'status', 'user_id'];
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
