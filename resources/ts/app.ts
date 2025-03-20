6274f8e9425729a56f312ad7b9bb9a129e6bb3fa
Cmilja <dsmiljanic40@gmail.com>
2025-03-20 19:24:04 +0100

    Dynamic task api function

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -1,1 +1,34 @@
 console.log("It's working!");
+
+const API_URl = 'http://127.0.0.1:8000/api/task';
+
+const containerDiv = document.querySelector('.container');
+
+const task = async (taskid?: number, method: string = 'GET', body?: object) => {
+    try {
+        let url = taskid ? `${API_URl}/${taskid}` : API_URl;
+
+        let options: RequestInit = {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+
+        if (body) {
+            options.body = JSON.stringify(body);
+        }
+
+        let response = await fetch(url, options);
+
+        if (!response.ok) {
+            console.log(response.status);
+            return;
+        }
+
+        let data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(`An error occured: ${error}`);
+    }
+};
