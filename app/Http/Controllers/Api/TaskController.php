22fb68a62a7543d99850f3559a832c224f078a24
Cmilja <dsmiljanic40@gmail.com>
2025-03-20 19:24:44 +0100

    TaskController

---
+++ app/Http/Controllers/Api/TaskController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\TaskRequest;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        return Task::paginate(5);
+    }
+
+    public function store(TaskRequest $request)
+    {
+        return Task::create($request->validated());
+    }
+
+    public function show(Task $task)
+    {
+        return $task;
+    }
+
+    public function update(TaskRequest $request, Task $task)
+    {
+        $task->update($request->validated());
+        return $task;
+    }
+
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return response()->json([
+            "message" => "Task deleted successfully"
+        ]);
+    }
+}
