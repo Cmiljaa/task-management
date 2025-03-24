4cbd24cca5b19ff2958174433e4facf73ed76e2f
Cmilja <dsmiljanic40@gmail.com>
2025-03-24 21:36:46 +0100

    Created task.ts file and moved task and loadTasks functions

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -1,6 +1,4 @@
-console.log("It's working!");
-
-const API_URl = 'http://127.0.0.1:8000/api/task';
+import { loadTasks, task } from '../ts/task';
 
 const containerDiv = document.querySelector('.container');
 const spinner = document.querySelector('.spinner') as HTMLElement;
@@ -23,33 +21,6 @@ interface TaskInfo extends Task {
 
 let currentPage = 1;
 
-const task = async (taskid?: number, method: string = 'GET', body?: object) => {
-    try {
-        let url = taskid ? `${API_URl}/${taskid}` : API_URl;
-
-        let options: RequestInit = {
-            method: method,
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        };
-
-        if (body) {
-            options.body = JSON.stringify(body);
-        }
-
-        let response = await fetch(url, options);
-
-        if (!response.ok) {
-            console.log(response.status);
-            return;
-        }
-
-        let data = await response.json();
-        return data;
-    } catch (error) {
-        console.log(`An error occured: ${error}`);
-    }
 const renderSpinner = () => {
     spinner?.classList.toggle('hidden');
     containerDiv?.classList.toggle('hidden');
