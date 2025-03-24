dd30926f57763f74b95f4d2d5acc7ea2f430cb87
Cmilja <dsmiljanic40@gmail.com>
2025-03-24 21:38:03 +0100

    Pagination added

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -34,6 +34,77 @@ window.addEventListener('load', async () => {
     renderTasks(tasks);
 });
 
+const paginationButtons = (tasks: any) => {
+    console.log(tasks);
+    prevPageBtn.disabled = !tasks.prev_page_url;
+    nextPageBtn.disabled = !tasks.next_page_url;
+
+    prevPageBtn.addEventListener('click', async () => {
+        if (currentPage > 1) currentPage--;
+
+        tasksPage();
+    });
+
+    nextPageBtn.addEventListener('click', async () => {
+        currentPage++;
+
+        tasksPage();
+    });
+};
+
+const viewTasks = () => {
+    document.querySelectorAll('.task-button')?.forEach((btn) => {
+        const taskId = Number(
+            btn.closest('.p-6')?.querySelector('.id')?.textContent
+        );
+        btn.addEventListener('click', async () => {
+            renderSpinner();
+            let taskInfo: TaskInfo = await task(taskId);
+            renderSpinner();
+            if (containerDiv) containerDiv.innerHTML = '';
+            containerDiv?.insertAdjacentHTML(
+                'afterbegin',
+                `
+            <div class="p-6 max-w-screen-lg mx-auto">
+            <button class="tasks inline-block px-4 py-2 text-sm rounded-md bg-blue-600 text-white mb-5 hover:bg-blue-700 transition duration-200">Back to tasks</button>
+        <div class="bg-white shadow-lg rounded-lg overflow-hidden mb-6 transform transition-all hover:shadow-xl border-2">
+            <div class="p-6">
+    
+                <div class="flex justify-between items-center">
+                    <h3 class="text-2xl text-gray-800">Task <span class="id">${
+                        taskInfo.id
+                    }</span></h3>
+                </div>
+
+                <h1 class="text-4xl font-bold">${taskInfo.title}</h1>
+                
+                <p class="text-gray-600 mt-2">${taskInfo.description}</p>
+    
+                <div class="mt-4">
+                    <span class="inline-block px-4 py-2 text-sm font-semibold rounded-md bg-blue-600 text-white">
+                        ${
+                            taskInfo.status.charAt(0).toUpperCase() +
+                            taskInfo.status.slice(1)
+                        }
+                    </span>
+                </div>
+    
+                <div class="mt-4 border-t border-gray-200 pt-4">
+                    <h4 class="text-lg font-semibold text-gray-800">Assigned to:</h4>
+                    <p class="text-gray-600">Name: ${taskInfo.user.name}</p>
+                    <p class="text-gray-600">Email: ${taskInfo.user.email}</p>
+                </div>
+            </div>
+        </div>
+    </div>`
+            );
+            document
+                .querySelector('.tasks')
+                ?.addEventListener('click', async () => tasksPage());
+        });
+    });
+};
+
 const renderTasks = (tasks: any) => {
     if (containerDiv) {
         containerDiv.innerHTML = '';
@@ -86,25 +157,5 @@ const renderTasks = (tasks: any) => {
 
 const renderSpinner = () => spinner?.classList.toggle('hidden');
 
-const paginationButtons = (tasks: any) => {
-    console.log(tasks);
-    prevPageBtn.disabled = !tasks.prev_page_url;
-    nextPageBtn.disabled = !tasks.next_page_url;
-
-    prevPageBtn.addEventListener('click', async () => {
-        if (currentPage > 1) currentPage--;
-
-        let pageTasks = await loadTasks(currentPage);
-        await renderTasks(pageTasks);
-        console.log(currentPage);
-    });
-
-    nextPageBtn.addEventListener('click', async () => {
-        currentPage++;
-
-        let pageTasks = await loadTasks(currentPage);
-        await renderTasks(pageTasks);
-        console.log(currentPage);
-    });
 };
 
