df0570ed7ac4d8d4a3ea63ca2ef743e654f67a31
Cmilja <dsmiljanic40@gmail.com>
2025-03-31 20:32:33 +0200

    Removed task function

--- resources/ts/task.ts
+++ resources/ts/task.ts
@@ -2,36 +2,15 @@ import axios from 'axios';
 import { Task, TaskInfo, TaskResponse } from './app';
 const API_URl = 'http://127.0.0.1:8000/api/task';
 
-export const task = async (
-    taskid?: number,
-    method: string = 'GET',
-    body?: object
-) => {
+export const storeTask = async (body: Task) => {
     try {
-        let url = taskid ? `${API_URl}/${taskid}` : API_URl;
+        console.log(body);
+        let response = await axios.post(API_URl, body);
 
-        let options: RequestInit = {
-            method: method,
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        };
-
-        if (body) {
-            options.body = JSON.stringify(body);
-        }
-
-        let response = await fetch(url, options);
-
-        if (!response.ok) {
-            console.log(response.status);
-            return;
-        }
-
-        let data = await response.json();
-        return data;
+        return response;
     } catch (error) {
         console.log(`An error occured: ${error}`);
+        return null;
     }
 };
 
@@ -40,7 +19,6 @@ export const loadTasks = async (page = 1): Promise<TaskResponse | null> => {
         let url = `${API_URl}/?page=${page}`;
 
         let response = await axios.get<TaskResponse>(url);
-        console.log(response);
 
         return response.data;
     } catch (error) {
@@ -52,7 +30,6 @@ export const loadTasks = async (page = 1): Promise<TaskResponse | null> => {
 export const loadTask = async (taskId: number): Promise<TaskInfo | null> => {
     try {
         let response = await axios.get<TaskInfo>(`${API_URl}/${taskId}`);
-        console.log(response);
         return response.data;
     } catch (error) {
         console.log(`An error occured: ${error}`);
