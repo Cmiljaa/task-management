7c6dd55a0bdcfcfdae97b3decd38c17dce6b6e1d
Cmilja <dsmiljanic40@gmail.com>
2025-03-31 20:34:39 +0200

    Fixed minor bugs

--- resources/ts/view.ts
+++ resources/ts/view.ts
@@ -1,5 +1,5 @@
-import { Task, TaskInfo, handleTaskClick, setupPaginationButtons } from './app';
-import { task } from './task';
+import { Task, TaskInfo, setupPaginationButtons, loadAndDisplayTask, loadAndDisplayTasks } from './app';
+import { storeTask, updateTask } from './task';
 
 const containerDiv = document.querySelector('.container');
 const spinner = document.querySelector('.spinner') as HTMLElement;
@@ -10,35 +10,31 @@ export const toggleSpinner = () => {
 };
 
 export const displayTasks = (tasks: any) => {
-    if (containerDiv) {
-        containerDiv.innerHTML = '';
-    }
+    if (containerDiv) containerDiv.innerHTML = '';
 
     console.log(tasks);
     tasks.data.forEach((task: Task) => {
-        let html = `<div class="p-2 max-w-screen-lg mx-auto">
+        containerDiv?.insertAdjacentHTML('beforeend', 
+            `<div class="p-2 max-w-screen-lg mx-auto">
                 <div class="bg-white shadow-lg rounded-lg overflow-hidden mb-6 transform transition-all hover:shadow-xl border-2">
                     <div class="p-6">
                         <div class="flex justify-between align-center items-center">
-                            <h3 class="text-lg font-bold text-gray-800">Task <span class="id">${
-                                task.id
-                            }</span> </h3>
+                            <h3 class="text-lg font-bold text-gray-800">
+                                Task <span class="id"> ${task.id} </span>
+                            </h3>
                         <p class="text-gray-600 mt-2">${task.description}</p>
-
                             <div class="mt-4">
-                            <span class="inline-block px-3 py-1 text-sm font-semibold rounded-md bg-gray-500 text-white">${
-                                task.status.charAt(0).toUpperCase() +
-                                task.status.slice(1)
-                            }</span>
+                                <span class="inline-block px-3 py-1 text-sm font-semibold rounded-md bg-gray-500 text-white">
+                                    ${task.status.charAt(0).toUpperCase() + task.status.slice(1)}
+                                </span>
                             </div>
                         </div>
 
                         <button class="task-button mt-4 cursor-pointer w-full bg-blue-600 text-white py-1.5 text-sm rounded-lg hover:bg-blue-700 transition duration-200">View Task</button>
                     </div>
                 </div>
-            </div>`;
-        containerDiv?.insertAdjacentHTML('beforeend', html);
-    });
+            </div>`);
+        });
 
     containerDiv?.insertAdjacentHTML(
         'beforeend',
@@ -57,11 +53,25 @@ export const displayTasks = (tasks: any) => {
     containerDiv?.insertAdjacentHTML(
         'afterbegin',
         `<div class="flex justify-center mb-5">
-        <button class="create-task mt-4 cursor-pointer w-4xl self-center justify-center bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition duration-200">Create Task</button>
-    </div>`
+            <button class="create-task mt-4 cursor-pointer w-4xl self-center justify-center bg-blue-600
+            text-white py-2 rounded-lg hover:bg-blue-700 transition duration-200">Create Task</button>
+        </div>`
     );
 
-    handleTaskClick();
+    document.querySelectorAll('.task-button')?.forEach((btn) => {
+        const taskId = Number(
+            btn.closest('.p-2')?.querySelector('.id')?.textContent
+        );
+
+        btn.addEventListener('click', async () => {
+            await loadAndDisplayTask(taskId);
+        });
+    });
+
+    document.querySelector('.create-task')?.addEventListener('click', () => {
+        displayCreateTask();
+        document.querySelector('.tasks')?.addEventListener('click', async () => await loadAndDisplayTasks());
+    });
 };
 
 export const displayTask = (taskInfo: TaskInfo) => {
@@ -69,15 +79,13 @@ export const displayTask = (taskInfo: TaskInfo) => {
     containerDiv?.insertAdjacentHTML(
         'afterbegin',
         `
-            <div class="p-6 max-w-screen-lg mx-auto">
-            <button class="tasks inline-block px-4 py-2 text-sm rounded-md bg-blue-600 text-white mb-5 hover:bg-blue-700 transition duration-200">Back to tasks</button>
+    <div class="p-6 max-w-screen-lg mx-auto">
+        <button class="tasks inline-block px-4 py-2 text-sm rounded-md bg-blue-600 text-white mb-5 hover:bg-blue-700 transition duration-200">Back to tasks</button>
         <div class="bg-white shadow-lg rounded-lg overflow-hidden mb-6 transform transition-all hover:shadow-xl border-2">
             <div class="p-6">
     
                 <div class="flex justify-between items-center">
-                    <h3 class="text-2xl text-gray-800">Task <span class="id">${
-                        taskInfo.id
-                    }</span></h3>
+                    <h3 class="text-2xl text-gray-800">Task <span class="id">${taskInfo.id}</span></h3>
                 </div>
 
                 <h1 class="text-4xl font-bold">${taskInfo.title}</h1>
@@ -86,10 +94,7 @@ export const displayTask = (taskInfo: TaskInfo) => {
     
                 <div class="mt-4">
                     <span class="inline-block px-4 py-2 text-sm font-semibold rounded-md bg-blue-600 text-white">
-                        ${
-                            taskInfo.status.charAt(0).toUpperCase() +
-                            taskInfo.status.slice(1)
-                        }
+                        ${taskInfo.status.charAt(0).toUpperCase() +taskInfo.status.slice(1)}
                     </span>
                 </div>
     
@@ -100,56 +105,64 @@ export const displayTask = (taskInfo: TaskInfo) => {
                 </div>
             </div>
         </div>
+        <button class="edit-task w-full bg-blue-600 text-white py-2 rounded-lg mt-4 hover:bg-blue-700 transition duration-200">
+            Edit Task
+        </button>
     </div>`
     );
+
+    document.querySelector('.edit-task')?.addEventListener('click', async () => {
+        displayEditTask(taskInfo);
+    })
+
+    document.querySelector('.tasks')?.addEventListener('click', async () => loadAndDisplayTasks());
 };
 
-export const createTask = () => {
+export const displayCreateTask = () => {
     if (containerDiv) containerDiv.innerHTML = '';
 
-    containerDiv?.insertAdjacentHTML(
-        'beforeend',
+    containerDiv?.insertAdjacentHTML('beforeend',
         `<div class="p-6 max-w-screen-lg mx-auto">
-    <button class="tasks inline-block px-4 py-2 text-sm rounded-md bg-blue-600 text-white mb-5 hover:bg-blue-700 transition duration-200">
-        Back to tasks
-    </button>
-    
-    <div class="bg-white shadow-lg rounded-lg overflow-hidden mb-6 transform transition-all hover:shadow-xl border-2">
-        <div class="p-6">
-
-            <!-- Title Input -->
-            <label class="block text-gray-700 text-sm font-bold mt-4" for="task-title">Title</label>
-            <input id="task-title" type="text" class="w-full border border-gray-300 rounded-md p-2 mt-1" value="" />
-
-            <!-- Description Input -->
-            <label class="block text-gray-700 text-sm font-bold mt-4" for="task-desc">Description</label>
-            <textarea id="task-desc" class="w-full border border-gray-300 rounded-md p-2 mt-1"></textarea>
-
-            <!-- Status Select -->
-            <label class="block text-gray-700 text-sm font-bold mt-4" for="task-status">Status</label>
-            <select id="task-status" class="w-full border border-gray-300 rounded-md p-2 mt-1">
-                <option value="pending" selected>Pending</option>
-                <option value="completed">Completed</option>
-            </select>
-
-            <button class="save-task w-full bg-green-600 text-white py-2 rounded-lg mt-4 hover:bg-green-700 transition duration-200">
-                Save Task
+            <button class="tasks inline-block px-4 py-2 text-sm rounded-md bg-blue-600 text-white mb-5 hover:bg-blue-700 transition duration-200">
+                Back to tasks
             </button>
-        </div>
-    </div>
-</div>`
-    );
+            
+            <div class="bg-white shadow-lg rounded-lg overflow-hidden mb-6 transform transition-all hover:shadow-xl border-2">
+                <div class="p-6">
 
-    document
-        .querySelector('.save-task')
-        ?.addEventListener('click', async () => {
-            let body: object = {
-                title: document.querySelector('#task-title')?.textContent,
-                description: document.querySelector('#task-desc')?.textContent,
-                status: document.querySelector('#task-status'),
+                    <label class="block text-gray-700 text-sm font-bold mt-4" for="task-title">Title</label>
+                    <input id="task-title" type="text" class="w-full border border-gray-300 rounded-md p-2 mt-1" value="" />
+
+                    <label class="block text-gray-700 text-sm font-bold mt-4" for="task-desc">Description</label>
+                    <textarea id="task-desc" class="w-full border border-gray-300 rounded-md p-2 mt-1"></textarea>
+
+                    <label class="block text-gray-700 text-sm font-bold mt-4" for="task-status">Status</label>
+                    <select id="task-status" class="w-full border border-gray-300 rounded-md p-2 mt-1">
+                        <option value="pending" selected>Pending</option>
+                        <option value="completed">Completed</option>
+                    </select>
+
+                    <button class="save-task w-full bg-green-600 text-white py-2 rounded-lg mt-4 hover:bg-green-700 transition duration-200">
+                        Save Task
+                    </button>
+                </div>
+            </div>
+        </div>`);
+
+    document.querySelector('.save-task')?.addEventListener('click', async () => {
+            let body: Task = {
+                title: (document.querySelector('#task-title') as HTMLInputElement)?.value,
+                description:(document.querySelector('#task-desc') as HTMLTextAreaElement)?.value ?? undefined,
+                status:(document.querySelector('#task-status') as HTMLSelectElement)?.value ?? 'pending',
                 user_id: 5,
             };
-            let data = await task(undefined, 'POST', body);
-            console.log(data);
-        });
+
+            let response = await storeTask(body);
+
+            loadAndDisplayTask(response?.data.id);
+
+            console.log(response, 'VIEW');
+    });
+
+    document.querySelector('.tasks')?.addEventListener('click', async () => loadAndDisplayTasks());
 };
